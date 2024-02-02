8dbeb204fa218cf29fd9fed2cb38182373e8b2d2
Mohamed Ali Farhat <100702507+MohamedAliFathat@users.noreply.github.com>
2024-02-02 18:32:12 +0000

    solution

---
+++ TableDeMultiplication.java
@@ -0,0 +1,39 @@
+class Main {
+    public static void main(String[] args)
+     {
+     
+     
+
+
+
+
+for (int j = 1; j<=20 ; j ++)
+
+{
+
+
+for (int i=1;i<=20;i++)
+{
+
+System.out.print(j*i);
+System.out.print(" ");
+
+}
+
+System.out.println(" ");
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
